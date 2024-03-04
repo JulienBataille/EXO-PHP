b77300e703aa28c16e82ca17073b424f0d31e471
Landesprit <julien.bataille40@gmail.com>
2024-03-04 16:24:05 +0100

    MAJ

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,22 @@
 <?php 
     include '../Config/database.php';
+    global $message ;
+    // Création du CRUD
 
-    // je vais créer un CRUD
+    // méthode pour supprimer un utilisateur
+    if(isset($_GET['method']) && $_GET['method'] == 'delete'){
+        if(isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $sql = "DELETE FROM user WHERE id=:id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+
+            $message = "<div class='alert alert-success text center'>L'utilisateur a été supprimé </div>";
+        }
+    }
+
+    //méthode pour lister les utilisateurs
     $sql = "SELECT * FROM user";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
@@ -24,12 +39,17 @@
 .tftable td {font-size:12px;border-width: 1px;padding: 8px;border-style: solid;border-color: #729ea5;}
 .tftable tr:hover {background-color:#ffffff;}
 </style>
-
+<h1>Users</h1>
+<?= $message ?>
 <table class="tftable" border="1">
-<tr><th>Id</th><th>Pseudo</th><th>Email</th><th>Né(e) le </th><th>Membre depuis le </th></tr>
+<tr><th>Id</th><th>Pseudo</th><th>Email</th><th>Né(e) le </th><th>Membre depuis le </th><th>Détails</th><th>Modifier</th><th>Supprimer</th></tr>
 
 <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {?>
-<tr><td><?= $row['id'] ?></td><td><?= $row['pseudo'] ?></td><td><?= $row['email'] ?></td><td><?= $row['birth_date'] ?></td><td><?= $row['created_at'] ?></td></tr>
+<tr><td><?= $row['id'] ?></td><td><?= $row['pseudo'] ?></td><td><?= $row['email'] ?></td><td><?= $row['birth_date'] ?></td><td><?= $row['created_at'] ?></td>
+    <td><a href="">Voir plus</a> </td>
+    <td><a href="">Modifier</a></td>
+    <td><a href="index.php?method=delete&id=<?=$row['id'] ?>">Supprimer</a> </td>
+</tr>
 <?php } ?>
 </table>
 
