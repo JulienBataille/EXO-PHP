f01f8618c4a53b1db88ca736436e33a4d7b262c8
Landesprit <julien.bataille40@gmail.com>
2024-03-05 15:08:46 +0100

    maj

--- admin/index_user_create.php
+++ admin/index_user_create.php
@@ -1,5 +1,64 @@
 <?php
-include '../Config/database.php'; ?>
+include '../Config/database.php'; 
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $avatar = $_FILES['avatar'];
+    $avatar_name = $avatar['name'];
+    $avatar_tmp_name = $avatar['tmp_name'];
+    $avatar_size = $avatar['size'];
+    $avatar_error = $avatar['error'];
+
+    $avatar_extension = explode('.', $avatar_name);
+    $avatar_extension = strtolower(end($avatar_extension));
+
+    $allowed_extension = ['jpg', 'png', 'jpeg'];
+
+    if(in_array($avatar_extension, $allowed_extension)){
+        if($avatar_error === 0){
+            if($avatar_size < 1000000){
+                $avatar_new_name = uniqid('',true).'.'.$avatar_extension;
+                $avatar_destination = '../assets/image/'.$avatar_new_name;
+                move_uploaded_file($avatar_tmp_name, $avatar_destination);
+            }
+        }
+    }
+
+echo $avatar_new_name;
+
+    $data = [
+        'pseudo'=> $_POST['pseudo'],
+        'email'=> $_POST['email'],
+        'password'=> password_hash($_POST['password'], PASSWORD_DEFAULT),
+        'birth_date'=> $_POST['birth_date'],
+        'gender'=> $_POST['gender'],
+        'is_valide'=> isset($_POST['is_valide']) ? 1 : 0,
+        'picture' => $avatar_new_name,
+        'created_at' => date('Y-m-d H:i:s')
+    ];
+
+    $sql="INSERT INTO user (pseudo, email, password, birth_date,gender, is_valide, picture, created_at)
+            VALUES (:pseudo, :email, :password, :birth_date,:gender, :is_valide, :picture, :created_at)";
+    
+
+    // $stmt = $req = $res
+    $req = $conn->prepare($sql);
+    $req->execute($data);
+
+    $form = "toto@toto.toto";
+    $subject = "votre compte a été créé";
+    $message = "Votre compte a été créé avec succès merci de changer votre mot de passe";
+    $header = "DE :".$form;
+
+    if(mail($_POST['email'], $subject, $message, $header)){
+        header('Location: index.php');
+    }
+
+
+}
+
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
